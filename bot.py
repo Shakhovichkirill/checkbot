dc43b19d0bc87ae672047ca5cf5551efa2c4b0e1
Shakhovichkirill <shakhovichkirill@gmail.com>
2018-11-01 18:08:39 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ class BotHandler:
 
     def __init__(self, token):
         self.token = token
-        self.api_url = "https://api.telegram.org/bot{}/".format(token)
+        self.api_url = "https://api.telegram.org/bot736351987:AAHYcVqjtaRwZ3tUA4tMM_WMB2w4eLMv4Ik/".format(token)
 
     def get_updates(self, offset=None, timeout=30):
         method = 'getUpdates'
